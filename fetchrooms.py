fe1d73272a4f8838593991754dbd94c32d7a22c2
Quentin Smith <quentin@mit.edu>
2021-11-23 05:32:30 -0500

    More queries

--- fetchrooms.py
+++ fetchrooms.py
@@ -15,10 +15,19 @@ else:
 user, _, password = n.authenticators("warehouse")
 conn = cx_Oracle.connect(user=user, password=password, dsn="warehouse", encoding="UTF-8")
 
-cursor = conn.cursor()
-cursor.execute("select * from space_detail where rownum < 100")
-columns = [col[0] for col in cursor.description]
-cursor.rowfactory = lambda *args: dict(zip(columns, args))
+out = dict()
 
-rooms = cursor.fetchall()
-print(yaml.dump(rooms))
+for key, query in {
+        "buildings": "select * from fac_building", # buildings has building_street_address
+        "floors": "select * from fac_floor",
+        "organizations": "select * from fac_organization",
+        "fac_rooms": "select * from fac_rooms",
+        "rooms": "select building_number, building_component, floor, room_number, building_room, building_room_name, room_square_footage, space_unit_code, space_unit, dlc_key, fclt_organization_key as organization_key, space_usage from space_detail left join buildings using (building_key) left join space_floor using (floor_key) left join space_unit using (space_unit_key) left join space_usage using (space_usage_key)",
+}.items():
+    cursor = conn.cursor()
+    cursor.execute(query)
+    columns = [col[0] for col in cursor.description]
+    cursor.rowfactory = lambda *args: {k.lower(): v for k,v in zip(columns, args) if k != 'WAREHOUSE_LOAD_DATE' and v != '' and v != None}
+
+    out[key] = cursor.fetchall()
+print(yaml.dump(out, Dumper=yaml.CDumper))
